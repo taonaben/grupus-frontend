d2feff77dbfdda4747f4e9ae5312de64a342bd5f
Taona Munikwa <69734394+taonaben@users.noreply.github.com>
2026-02-26 23:11:01 +0200

    hmm

--- lib/features/chat/views/chat_screen.dart
+++ lib/features/chat/views/chat_screen.dart
@@ -5,6 +5,13 @@ class ChatScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chat Screen'),
+      ),
+      body: const Center(
+        child: Text('This is the chat screen.'),
+      ),
+    );
   }
 }
